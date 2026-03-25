13feb63417e6275e73eae55717fa7ffa31cbbd0c
Yato-Coding-Stuff <265033978+Yato-Coding-Stuff@users.noreply.github.com>
2026-03-25 18:26:50 +0100

    full list_command implementation

---
+++ src/cli/commands/list_command.rs
@@ -0,0 +1,78 @@
+use crate::util::context::Context;
+
+pub fn list(ctx: &mut Context, packages: Vec<String>, available: bool, all_versions: bool) {
+    if available {
+        list_available(ctx, packages, all_versions);
+    } else {
+        list_installed(ctx, packages);
+    }
+}
+
+pub fn list_available(ctx: &mut Context, queries: Vec<String>, all_versions: bool) {
+    let matches: Vec<_> = ctx
+        .index
+        .all_versions_by_full_name
+        .iter()
+        .filter(|(package_name, _)| {
+            queries.is_empty()
+                || queries
+                    .iter()
+                    .any(|q| package_name.to_lowercase().contains(&q.to_lowercase()))
+        })
+        .collect();
+
+    if matches.is_empty() {
+        for query in queries {
+            println!("==> No matches found for '{}'", query);
+        }
+        return;
+    }
+
+    for (package_name, versions) in matches {
+        if !queries.is_empty()
+            && !queries
+                .iter()
+                .any(|q| package_name.to_lowercase().contains(&q.to_lowercase()))
+        {
+            continue;
+        }
+
+        if all_versions {
+            println!("==> {}", package_name);
+            for ver in versions {
+                println!("-> {}", ver.package_full_name_with_version);
+            }
+        } else if let Some(latest) = versions
+            .iter()
+            .max_by(|a, b| a.version_number.cmp(&b.version_number))
+        {
+            println!("==> {}", latest.package_full_name_with_version);
+        }
+    }
+}
+
+pub fn list_installed(ctx: &mut Context, query: Vec<String>) {
+    let all_packages = ctx.tracker.get_all();
+    let filtered: Vec<_> = all_packages
+        .values()
+        .filter(|p| {
+            query.is_empty()
+                || query.iter().any(|q| {
+                    p.package_full_name
+                        .to_lowercase()
+                        .contains(&q.to_lowercase())
+                })
+        })
+        .collect();
+
+    if filtered.is_empty() {
+        for query in query {
+            println!("==> No matches found for '{}'", query);
+        }
+        return;
+    }
+
+    for pkg in filtered {
+        println!("==> {}", pkg.package_full_name_with_version);
+    }
+}
